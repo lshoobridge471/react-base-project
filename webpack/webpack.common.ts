547fff249d8e9079c60b9474a43a7c302bcd1a95
Lucas Shoobridge <shoobridgelucas@hotmail.com>
2021-03-08 11:20:23 -0300

    Webpack ordered.

--- webpack/webpack.common.ts
+++ webpack/webpack.common.ts
@@ -1,19 +1,12 @@
-import * as path from "path";
-import * as webpack from "webpack";
+import { resolve } from "path";
+import { Configuration } from "webpack";
 import * as HtmlWebPackPlugin from "html-webpack-plugin";
 import * as MiniCSSExtractPlugin from 'mini-css-extract-plugin';
 
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/public/index.html",
-  filename: "./index.html"
-});
-
-const config: webpack.Configuration = {
-  mode: "production",
-  devtool: 'cheap-module-source-map',
+export const common: Configuration = {
   entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
   resolve: {
@@ -37,7 +30,7 @@ const config: webpack.Configuration = {
             options: {
                 resources: [
                     // The second parameter of the resolve method is the address of the scss configuration file. If there are more than one, just add them one by one.
-                    path.resolve(__dirname, './../src/theme.scss')
+                    resolve(__dirname, './../src/theme.scss')
                 ]
             }
         }
@@ -46,11 +39,12 @@ const config: webpack.Configuration = {
     ]
   },
   plugins: [
-    htmlPlugin,
+    new HtmlWebPackPlugin({
+        template: "./src/public/index.html",
+        filename: "./index.html"
+    }),
     new MiniCSSExtractPlugin({
       filename: "./css/styles.css",
     })
   ]
 };
-
-export default config;
---
+++ webpack/webpack.dev.ts
@@ -0,0 +1,11 @@
+import { Configuration } from "webpack";
+import { merge } from 'webpack-merge';
+
+import { common } from './webpack.common';
+
+const config: Configuration = merge(common, {
+  mode: "development",
+  devtool: 'eval-source-map',
+});
+
+export default config;
