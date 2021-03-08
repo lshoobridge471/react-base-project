4913e3c9fbc36c909952b66f75d2e2e649d9d88a
Lucas Shoobridge <shoobridgelucas@hotmail.com>
2021-03-08 11:08:10 -0300

    Add node-sass & global variables.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import './index.scss';
 
 const App = () => <h1>React Typescript App</h1>
 
--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import * as webpack from "webpack";
 import * as HtmlWebPackPlugin from "html-webpack-plugin";
+import * as MiniCSSExtractPlugin from 'mini-css-extract-plugin';
 
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./src/public/index.html",
@@ -23,10 +24,33 @@ const config: webpack.Configuration = {
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { test: /\.css$/, loader: "css-loader" },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          "css-loader",
+          'sass-loader',
+          {
+            loader: 'sass-resources-loader',
+            options: {
+                resources: [
+                    // The second parameter of the resolve method is the address of the scss configuration file. If there are more than one, just add them one by one.
+                    path.resolve(__dirname, './../src/theme.scss')
+                ]
+            }
+        }
+      ]
+    },
     ]
   },
-  plugins: [htmlPlugin]
+  plugins: [
+    htmlPlugin,
+    new MiniCSSExtractPlugin({
+      filename: "./css/styles.css",
+    })
+  ]
 };
 
 export default config;
