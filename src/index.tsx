d05cc6c650cd27b745f1579ff0ad75a832bdd1aa
Lucas Shoobridge <shoobridgelucas@hotmail.com>
2021-03-08 00:17:05 -0300

    Initial commit. React + Typescript + Webpack

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const App = () => <h1>React Typescript App</h1>
+
+ReactDOM.render(
+	<App />,
+  	document.getElementById("root")
+);
